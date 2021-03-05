f226b20fd9da574cc92b4af061a8b28f5657f629
iambuoyant <imkhaaan@gmail.com>
2021-03-06 03:40:04 +0500

    port fixed

--- source/config/config.ts
+++ source/config/config.ts
@@ -43,7 +43,7 @@ const MONGO = {
 };
 
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || 'localhost';
-const SERVER_PORT = process.env.SERVER_PORT || 4000;
+const SERVER_PORT = process.env.PORT || 4000;
 
 const SERVER = {
     hostname: SERVER_HOSTNAME,
